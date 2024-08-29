cf3a327d454a1f5dfc5d268a5024c2c21f230e0b
Larissa Böhm Araújo <145381988+lalabohm@users.noreply.github.com>
2024-08-29 19:18:55 -0300

    Update Main.java

--- CalculoImposto/src/main/java/Main.java
+++ CalculoImposto/src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Massa de dados fixa (hard-coded)
         Item produto = new Item("Computador", 3000.00, true);
         Item servico = new Item("Consultoria", 1500.00, false);
 
@@ -10,8 +9,7 @@ public class Main {
 
         OperacaoComercial operacaoProduto = new OperacaoComercial(produto, iss, icms, ipi);
         OperacaoComercial operacaoServico = new OperacaoComercial(servico, iss, icms, ipi);
-
-        // Mostrar detalhes das operações
+        
         System.out.println("Detalhes da Operação Comercial - Produto:");
         operacaoProduto.mostrarDetalhes();
         System.out.println();
